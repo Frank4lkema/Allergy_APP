b87aa3aedae2e69812c213c24d8418e4e9f265a9
Frank Alkema <frank.alkema@icloud.com>
2018-11-26 16:05:22 +0100

    added a uniquess to the allergies table

--- app/models/allergy.rb
+++ app/models/allergy.rb
@@ -1,6 +1,6 @@
 class Allergy < ApplicationRecord
   # Validations
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :allergy_information, presence: true
 
   # Associations
