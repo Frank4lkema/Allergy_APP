06d8fee8bd2c643de25ecfcfdaec22aa647008d4
Frank Alkema <frank.alkema@icloud.com>
2018-11-26 16:47:04 +0100

    made the seed file work

--- app/models/allergy.rb
+++ app/models/allergy.rb
@@ -1,7 +1,7 @@
 class Allergy < ApplicationRecord
   # Validations
   validates :name, presence: true, uniqueness: true
-  validates :allergy_information, presence: true
+  # validates :allergy_information, presence: true
 
   # Associations
   has_many :user_allergies
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "this works"
+allergies = ["Lijnzaad","Sesamzaad","Perzik","Banaan","Avocado","Kiwi","Passievrucht","Selderij","Knoflook","Mosterdzaden","Anijszaad","Kamille","Vis","Schelpdieren","Koemelk","Tarwe","Soja","Pinda’s","Boomnoten", "Eieren"]
+
+puts "this works aswell"
+allergies.each do |allergy|
+  Allergy.create!(name: allergy)
+end
+
+puts "it doesn't break here"
+
+
+
