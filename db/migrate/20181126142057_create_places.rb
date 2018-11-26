36f4ab902ac7649d6ea4675ea65d2aee42d260db
Frank Alkema <frank.alkema@icloud.com>
2018-11-26 15:22:26 +0100

    now with the right colums

--- db/migrate/20181126142057_create_places.rb
+++ db/migrate/20181126142057_create_places.rb
@@ -1,6 +1,8 @@
 class CreatePlaces < ActiveRecord::Migration[5.2]
   def change
     create_table :places do |t|
+      t.string :country
+      t.string :language
 
       t.timestamps
     end
--- db/migrate/20181126142200_create_allergies.rb
+++ db/migrate/20181126142200_create_allergies.rb
@@ -1,6 +1,8 @@
 class CreateAllergies < ActiveRecord::Migration[5.2]
   def change
     create_table :allergies do |t|
+      t.string :name
+      t.text :allergy_information
 
       t.timestamps
     end
