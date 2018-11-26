d37edcabf0ed393e88f1c8fda497fedff821fab4
Frank Alkema <frank.alkema@icloud.com>
2018-11-26 15:34:19 +0100

    made schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_114601) do
+ActiveRecord::Schema.define(version: 2018_11_26_143129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "allergies", force: :cascade do |t|
+    t.string "name"
+    t.text "allergy_information"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "country"
+    t.string "language"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "translated_allergies", force: :cascade do |t|
+    t.bigint "allergy_id"
+    t.string "name"
+    t.string "language"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["allergy_id"], name: "index_translated_allergies_on_allergy_id"
+  end
+
+  create_table "user_allergies", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "allergy_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["allergy_id"], name: "index_user_allergies_on_allergy_id"
+    t.index ["user_id"], name: "index_user_allergies_on_user_id"
+  end
+
+  create_table "user_places", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_user_places_on_place_id"
+    t.index ["user_id"], name: "index_user_places_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +68,9 @@ ActiveRecord::Schema.define(version: 2018_11_26_114601) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "translated_allergies", "allergies"
+  add_foreign_key "user_allergies", "allergies"
+  add_foreign_key "user_allergies", "users"
+  add_foreign_key "user_places", "places"
+  add_foreign_key "user_places", "users"
 end
