d157173393f3dafcdbaef3f8e9f6f8f70d5e839e
Frank Alkema <frank.alkema@icloud.com>
2018-12-03 11:24:22 +0100

    made the allergies index

--- app/controllers/user_places_controller.rb
+++ app/controllers/user_places_controller.rb
@@ -1,11 +1,14 @@
 class UserPlacesController < ApplicationController
-
   def create
     @user_place = UserPlace.new
     place = Place.search_place(params[:store])
     @user_place.place_id = place.id
     @user_place.user_id = current_user.id
-    @user_place.save!
-    raise
+
+    if @user_place.save
+      redirect_to allergies_path
+    else
+      render root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "search", to: "users#search", as: :search
   resources :user_places, only: [:create]
+  resources :allergies, only: [:index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
