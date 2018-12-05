63204bf219dc2a1a573b8c9c5962504d2d7079f8
Frank Alkema <frank.alkema@icloud.com>
2018-12-05 20:20:30 +0100

    almost done with the translate method

--- app/controllers/translated_allergies_controller.rb
+++ app/controllers/translated_allergies_controller.rb
@@ -2,7 +2,7 @@ class TranslatedAllergiesController < ApplicationController
 
   def new
     puts "this works"
-    result = translate(current_user)
+    TranslatedAllergy.translate(current_user)
     raise
   end
 end
--- app/models/translated_allergy.rb
+++ app/models/translated_allergy.rb
@@ -4,17 +4,14 @@ class TranslatedAllergy < ApplicationRecord
 
   # Associations
   belongs_to :allergy
-  def translate(current_user)
+  def self.translate(current_user)
     puts "works "
     base_url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key="
     api_key = ENV['TRANSLATE_KEY']
     language_code = current_user.user_places.last.place.language_code
     allergy = current_user.user_allergies.last.allergy.name
-    url = "#{base_url}?key=#{api_key}&text=#{allergy}&lang=#{language_code}"
-    puts url
-    puts url
-    puts url
-    puts "this works"
+    url = "#{base_url}#{api_key}&text=#{allergy}&lang=en-#{language_code}"
+    data = JSON.parse(open(url).read)
     binding.pry
   end
 end
