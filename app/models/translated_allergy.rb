2acf45d65cef8b1585d29fadc1c892ee700fd115
Frank Alkema <frank.alkema@icloud.com>
2018-12-06 09:40:39 +0100

    made it all work

--- app/models/translated_allergy.rb
+++ app/models/translated_allergy.rb
@@ -5,13 +5,12 @@ class TranslatedAllergy < ApplicationRecord
   # Associations
   belongs_to :allergy
   def self.translate(current_user)
-    puts "works "
     base_url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key="
     api_key = ENV['TRANSLATE_KEY']
     language_code = current_user.user_places.last.place.language_code
     allergy = current_user.user_allergies.last.allergy.name
     url = "#{base_url}#{api_key}&text=#{allergy}&lang=en-#{language_code}"
     data = JSON.parse(open(url).read)
-    binding.pry
+    data["text"][0]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :user_places, only: [:create]
   resources :allergies, only: [:index]
   resources :user_allergies, only: [:create]
-  resources :translated_allergies, only: [:new]
+  resources :translated_allergies, only: [:new, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
