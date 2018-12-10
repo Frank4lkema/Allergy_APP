249c8318b9dd587fd3e218067114990263120e03
Frank Alkema <frank.alkema@icloud.com>
2018-12-10 11:03:42 +0100

    moved the right action to the model

--- app/models/translated_allergy.rb
+++ app/models/translated_allergy.rb
@@ -11,6 +11,9 @@ class TranslatedAllergy < ApplicationRecord
     allergy = current_user.user_allergies.last.allergy.name
     url = "#{base_url}#{api_key}&text=#{allergy}&lang=en-#{language_code}"
     data = JSON.parse(open(url).read)
-    data["text"][0]
+    result = data["text"][0]
+    country = current_user.user_places.last.place.language
+    allergy_id = current_user.user_allergies.last.allergy.id
+    TranslatedAllergy.new(name: result, language: country, allergy_id: allergy_id)
   end
 end
